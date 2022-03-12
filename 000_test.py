de8481eaf105b9f07e4a340083288e55f07c1b88
giadarol <giovanni.iadarola@cern.ch>
2022-03-12 21:19:22 +0100

    Automatic check

--- 000_test.py
+++ 000_test.py
@@ -17,8 +17,18 @@ pyk2.pyk2(x_particles=x_test,
           s_particles=s_test,
           p_particles=p_test)
 
-x_ref = np.loadtxt('./rcx.dump_after_REF')
+x_ref = np.loadtxt("./rcx.dump_after_REF")
+xp_ref = np.loadtxt("./rcxp.dump_after_REF")
+y_ref = np.loadtxt("./rcy.dump_after_REF")
+yp_ref = np.loadtxt("./rcyp.dump_after_REF")
+s_ref = np.loadtxt("./rcs.dump_after_REF")
+p_ref = np.loadtxt("./rcp.dump_after_REF")
 
-assert np.allclose(x_test, x_ref, rtol=1e6, atol=0)
+assert np.allclose(x_test, x_ref, atol=1e-9, rtol=0)
+assert np.allclose(xp_test, xp_ref, atol=5e-9, rtol=0)
+assert np.allclose(y_test, y_ref, atol=1e-9, rtol=0)
+assert np.allclose(yp_test, yp_ref, atol=5e-9, rtol=0)
+assert np.allclose(s_test, s_ref, atol=2e-4, rtol=0)
+assert np.allclose(p_test, p_ref, atol=0, rtol=1e-7)
 
 
