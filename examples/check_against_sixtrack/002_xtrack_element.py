dd69c9dca128bc02d12ced35da1230258f651949
giadarol <giovanni.iadarola@cern.ch>
2022-03-17 07:56:43 +0100

    More checks

--- examples/check_against_sixtrack/002_xtrack_element.py
+++ examples/check_against_sixtrack/002_xtrack_element.py
@@ -38,7 +38,9 @@ px_part = part.px.copy(); px_part[part_id]=part.px
 y_part = part.y.copy(); y_part[part_id]=part.y
 py_part = part.py.copy(); py_part[part_id]=part.py
 delta_part = part.delta.copy(); delta_part[part_id]=part.delta
-zeta_part = part.delta.copy(); zeta_part[part_id]=part.delta
+psigma_part = part.psigma.copy(); psigma_part[part_id]=part.psigma
+zeta_part = part.zeta.copy(); zeta_part[part_id]=part.delta
+rpp_part = part.rpp.copy(); rpp_part[part_id]=part.rpp
 
 x_ref = np.loadtxt("./rcx.dump_after_REF")
 xp_ref = np.loadtxt("./rcxp.dump_after_REF")
@@ -56,4 +58,11 @@ assert np.all(py_part[state_part<1] == part_test.py[state_part<1])
 assert np.all(delta_part[state_part<1] == part_test.delta[state_part<1])
 assert np.all(zeta_part[state_part<1] == part_test.zeta[state_part<1])
 
-
+xp_part = px_part*rpp_part
+yp_part = py_part*rpp_part
+p_part = (psigma_part*part.energy0 + part.energy0)/1e9
+assert np.allclose(x_part[state_part>0], x_ref[state_part>0], atol=1e-9, rtol=0)
+assert np.allclose(xp_part[state_part>0], xp_ref[state_part>0], atol=5e-9, rtol=0)
+assert np.allclose(y_part[state_part>0], y_ref[state_part>0], atol=1e-9, rtol=0)
+assert np.allclose(yp_part[state_part>0], yp_ref[state_part>0], atol=5e-9, rtol=0)
+assert np.allclose(p_part[state_part>0], p_ref[state_part>0], atol=0, rtol=1e-7)
